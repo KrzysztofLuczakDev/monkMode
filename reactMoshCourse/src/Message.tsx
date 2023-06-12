b4c6fa0b735c234c720fc1f6f5976f2f7205addf
Krzysztof Luczak <118281016+KrzysztofLuczakDev@users.noreply.github.com>
2023-06-12 08:24:08 +0200

    done moshCourse

---
+++ reactMoshCourse/src/Message.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+
+const Message = () => {
+
+  const [first, setFirst] = useState(second)  
+  return (
+    <div>Message</div>
+  )
+}
+
+export default Message
